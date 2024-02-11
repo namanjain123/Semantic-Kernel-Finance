26ac387a2436adcfd863d18ff8712db58e01c5f6
namanjain123 <naman.nishi@gmail.com>
2024-02-12 00:13:55 +0530

    fixes to error in file conversion

--- Semantic-Kernel-RAG-Finance/API/utils/BaseConverter.cs
+++ Semantic-Kernel-RAG-Finance/API/utils/BaseConverter.cs
@@ -1,24 +1,30 @@
 using Domain.Interfaces;
 using Models.Chat;
 
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace API.utils;
 
 public static class BaseConverter
 {
     public static IFormFileCollection ConvertToFormFileCollection(List<FileInput> fileInputs)
-{
-    var formFiles = fileInputs.Select(fileInput => new FormFile(
-        new MemoryStream(Convert.FromBase64String(fileInput.Content)),
-        0, // Placeholder for length, as it's not used in your example
-        Convert.FromBase64String(fileInput.Content).Length,
-        fileInput.Name,
-        fileInput.Name)
     {
-        Headers = new HeaderDictionary(),
-        ContentType = fileInput.Type
-    }).ToList();
-
-    return new FormFileCollection(formFiles);
-}
+        var formFiles = fileInputs.Select(fileInput => new FormFile(
+            new MemoryStream(Convert.FromBase64String(fileInput.Content)),
+            0, // Placeholder for length, as it's not used in your example
+            Convert.FromBase64String(fileInput.Content).Length,
+            fileInput.Name,
+            fileInput.Name)
+        {
+            Headers = new HeaderDictionary(),
+            ContentType = fileInput.Type
+        }).ToList();
+        var result=new FormFileCollection();
+        result.AddRange(formFiles);
+        return result;
+    }
 
 }
